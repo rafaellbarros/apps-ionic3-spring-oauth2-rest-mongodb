f5b16bd178e9a3c75fa5eeb4c74554716a638971
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-10 11:13:01 -0300

    :construction: OAuth2 na prática

---
+++ secao-03/10-oauth2-na-pratica/spring-aula-07/src/main/java/io/spring/aula/rafael/config/MyUserDetails.java
@@ -0,0 +1,51 @@
+package io.spring.aula.rafael.config;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.spring.aula.rafael.entity.Usuario;
+
+public class MyUserDetails extends Usuario implements UserDetails{
+
+    public MyUserDetails(Usuario usuario) {
+        super(usuario);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getPerfis();
+    }
+
+    @Override
+    public String getPassword() {
+        return getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
