fef92c78740c8cf52b52d82d1fa8acc9a20d8770
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-13 20:47:29 -0300

    :construction: Iniciando aplicação com jar

--- secao-04/14-cookies-manipular-o-access-token/spring-aula-11/src/main/java/io/spring/aula/rafael/Aplicacao.java
+++ secao-04/14-cookies-manipular-o-access-token/spring-aula-11/src/main/java/io/spring/aula/rafael/Aplicacao.java
@@ -2,6 +2,7 @@ package io.spring.aula.rafael;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
