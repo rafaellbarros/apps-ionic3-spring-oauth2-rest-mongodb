53b70a0ecdbb1c7e5149173459b4449105a81efa
Rafael Barros <rafaelbarros.df@gmail.com>
2019-04-07 18:42:28 -0300

    :construction: Atualizando urlBackEnd

--- secao-06/21-implementar-refresh-token-com-ionic/oauth2/ionic3-aula-17/src/entity/Utils.ts
+++ secao-06/21-implementar-refresh-token-com-ionic/oauth2/ionic3-aula-17/src/entity/Utils.ts
@@ -1,7 +1,7 @@
 export class Utils {
     static get urlBackEnd() { 
-        //return 'https://spring-aula-14.herokuapp.com';
-        return 'http://localhost:8080';
+        return 'https://spring-aula-16.herokuapp.com';
+        //return 'http://localhost:8080';
     }
 
     static json = (obj: any) => JSON.parse(JSON.stringify(obj));
